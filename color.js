a3ac02dfbba54686019a498f1bd5b34e7c45762a
BWade <BranWade@gmail.com>
2019-05-15 23:37:13 -0700

    Code Broken

--- color.js
+++ color.js
@@ -10,23 +10,25 @@ var colors = [
   var pickedColor= pickColor();
   var colorDisplay = document.getElementById("colorDisplay");
   var messageDisplay = document.querySelector("#message");
+  var h1 = document.querySelector("h1");
 
   colorDisplay.textContent = pickedColor;
 
   for(var i = 0; i < squares.length; i++){
   	//add initial colors to squares
-  	squares[i].style.backgroundColor = colors[i]
+  	squares[i].style.background = colors[i]
 
   	//add click listeners to squares
   	squares[i].addEventListener("click", function(){
   		//grab color of cliked square
-  		var clickedColor = this.style.backgroundColor;
+  		var clickedColor = this.style.background;
   		// compare color to pickedColor
   		if(clickedColor === pickedColor){
   			messageDisplay.textContent = "Correct";
   			changeColors(clickedColor);
+  			h1.style.background = clickedColor;
   		} else {
-  			this.style.backgroundColor = "#232323";
+  			this.style.background = "#232323";
   			messageDisplay.textContent = "Try Again"
   		}
 
@@ -47,3 +49,22 @@ var colors = [
   	var random = Math.floor(Math.random() * colors.length);
   	return colors[random];
   }
+
+  function generateRandomColors(num){
+  	//make an array
+  	var arr = []
+  	//add num random color to array
+  	for(var i = 0; i < num; i++){
+  	  	//get random color and push into arr
+		arr.push(randomColor())
+}
+  		//return that array
+  		return arr;
+  }
+function randomColor(){
+//pick a RGB from 0 - 255
+ var r = Math.floor(Math.random() * 256 );
+ var g = Math.floor(Math.random() * 256 );
+ var b = Math.floor(Math.random() * 256 );
+ return "rgb(" + r + ", " + g + ", " + b + ")";
+}
